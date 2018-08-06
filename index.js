574967579ee658cdc5e4669cf1a6bd1e995b9d3a
Flavio Takeuchi <flavio.tak89@gmail.com>
2018-08-06 16:50:16 -0300

    Conversão para promise Atualização de README

--- index.js
+++ index.js
@@ -1,41 +1,45 @@
-'use strict';
+'use strict'
 
-var fs = require('fs');
-var intoStream = require('into-stream');
+var fs = require('fs')
+var intoStream = require('into-stream')
 
-function encode(path, callback) {
-  var rs = fs.createReadStream(path, {
-    encoding: 'base64'
-  });
-
-  var str = '';
-  rs.on('data', function(chuck) {
-    str += chuck;
-  });
-
-  rs.on('end', function() {
-    callback(null, str);
-  });
-
-  rs.on('error', function() {
-    callback('encode fail');
+function encode(path) {
+  return new Promise((resolve, reject) => {
+    var rs = fs.createReadStream(path, {
+      encoding: 'base64'
+    })
+  
+    var str = ''
+    rs.on('data', function(chuck) {
+      str += chuck
+    })
+  
+    rs.on('end', function() {
+      resolve(str)
+    })
+  
+    rs.on('error', function() {
+      reject('encode fail')
+    })
   })
 }
 
-function decode(base64String, output, callback) {
-  var bitmap = new Buffer(base64String, 'base64');
-  var rs = intoStream(bitmap)
-  var ws = fs.createWriteStream(output);
+function decode(base64String, output) {
+  return new Promise((resolve, reject) => {
+    var bitmap = new Buffer(base64String, 'base64')
+    var rs = intoStream(bitmap)
+    var ws = fs.createWriteStream(output)
 
-  rs.on('end', function() {
-    callback(null, output);
-  });
+    rs.on('end', function() {
+      resolve(output)
+    })
 
-  rs.on('error', function() {
-    callback('decode fail');
-  });
+    rs.on('error', function() {
+      reject('decode fail')
+    })
 
-  rs.pipe(ws);
+    rs.pipe(ws)
+  })
 }
 
 module.exports = {
